cd708f0c6c2d57d9ad2388993b6bc412236abc3d
Raissa Queiroz <raissaqueirozteixeira@gmail.com>
2019-11-16 17:53:39 -0300

    inserindo arquivos do githooks

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
+export default function App(){
+    const [ repositories, setRepositories ] = useState([]);
+
+    useEffect(async () => {
+        const response = await fetch('https://api.github.com/users/raissaqueiroz/repos');
+        const data = await response.json();
+        setRepositories(data);
+    }, []);
+
+    useEffect(() => {
+        const filtered = repositories.filter(repo => repo.fav);
+
+        document.title = `Você tem ${filtered.length} favoritos!`; 
+    }, [repositories]);
+    
+    function handleFavorite(id){
+        const newRepositories = repositories.map(repo => {
+            return repo.id === id ? { ...repo, fav: !repo.fav } : repo;
+        });
+
+        setRepositories(newRepositories);
+    }
+
+    return(
+        <ul>
+            {repositories.map(repo => (
+                <li key={repo.id}>
+                    {repo.name} {repo.fav && <span>Fav</span>}
+                    <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+                </li>
+            ))}
+        </ul> 
+    );
+}
+
+
